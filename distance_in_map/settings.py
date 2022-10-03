7694e51a67948c0a54582b24869a49cd40574de5
ahsouleymane <souleymaneah98@gmail.com>
2022-10-03 16:48:10 +0100

    deploy in local

--- distance_in_map/settings.py
+++ distance_in_map/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=$6nkizzn_vrv-&(zrxoc7qin^(!leas1be-^t5e)6jqu*#5u8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.20.240']
 
 
 # Application definition
--- measurement/views.py
+++ measurement/views.py
@@ -5,9 +5,36 @@ from geopy.geocoders import Nominatim
 from geopy.distance import geodesic # this fonction calculate distance
 from .utils import get_geo, get_center_coordinates, get_zoom, get_ip_address
 import folium
+import requests
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from ipware import get_client_ip
+import json, urllib
 # Create your views here.
 
+class locationAPI(APIView):
+
+    def get(self, request, format=None):
+        client_ip, ip_routable = get_client_ip(request)
+
+        if client_ip is None:
+            client_ip = "0.0.0.0"
+        else:
+            if ip_routable:
+                ip_type = "public"
+            else:
+                ip_type = "private"
+
+        print(client_ip, ip_type)
+
+        ip_address = '154.127.94.179'
+        url='https://api.ipfind.com/?ip=' + ip_address
+        respon = urllib.request.urlopen(url)
+        data1 = json.loads(respon.read())
+        data1['client_ip'] = client_ip
+        data1['ip_type'] = ip_type
+        return Response(data1)
 
 
 def calculate_distance_view(request):
@@ -23,23 +50,42 @@ def calculate_distance_view(request):
 
     ip_ = get_ip_address(request)
     print(ip_)
-    ip = '72.14.207.99'
+    ip = '154.127.94.179'
 
     country, city, lat, lon = get_geo(ip)
 
     location = geolocator.geocode(city)
 
+    # Location coordinates using json
+    r = requests.get('https://get.geojs.io/')
+    ip_request = requests.get('https://get.geojs.io/v1/ip.json')
+    ip_add = ip_request.json()['ip']
+    print(ip_add)
+
+    url = 'https://get.geojs.io/v1/ip/geo/'+ip_add+'.json'
+    geo_request = requests.get(url)
+    geo_data = geo_request.json()
+    print(geo_data)
+
+    x_lat = geo_data['latitude']
+    x_lon = geo_data['longitude']
+
+    print(x_lat, x_lon)
+
+
     # Location coordinates
     l_lat = lat
     l_lon = lon
 
+    #print(l_lat, l_lon)
+
     pointA = (l_lat, l_lon)
 
     # Initial folium map
-    m = folium.Map(width=1600, height=600, location=get_center_coordinates(l_lat, l_lon), zoom_start=8)
+    m = folium.Map(width=1600, height=600, location=get_center_coordinates(x_lat, x_lon), zoom_start=8)
 
     # Location marker
-    folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'], 
+    folium.Marker([x_lat, x_lon], tooltip='click here for more', popup=city['city'], 
                     icon=folium.Icon(color='purple')).add_to(m)
 
     if form.is_valid():
@@ -61,7 +107,7 @@ def calculate_distance_view(request):
             zoom_start=get_zoom(distance))
 
         # Location marker
-        folium.Marker([l_lat, l_lon], tooltip='click here for more', popup=city['city'], 
+        folium.Marker([x_lat, x_lon], tooltip='click here for more', popup=city['city'], 
                         icon=folium.Icon(color='purple')).add_to(m)
 
         # Destination marker
